3cee1add0af7b291d927934b87d628a67820e431
Raghav Chauhan <raghavchauhan3347@gmail.com>
2025-03-27 20:10:13 +0530

    Initial commit

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Sidebar() {
+  return (
+    <aside className="sidebar">
+      <ul>
+        <li className="active">Details</li>
+      </ul>
+    </aside>
+  );
+}
+
+export default Sidebar;
